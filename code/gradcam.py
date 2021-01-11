7a5ec7bc47ac05fa6cb62bc62b62dad6fb827989
Vivien van Veldhuizen <vivien.van.veldhuizen@hotmail.com>
2021-01-11 10:51:12 +0100

    Commented part of gradcam

--- code/gradcam.py
+++ code/gradcam.py
@@ -24,6 +24,7 @@ class PropBase(object):
         self.outputs_forward = OrderedDict()
         self.set_hook_func()
 
+
     def set_hook_func(self):
         raise NotImplementedError
 
@@ -34,8 +35,10 @@ class PropBase(object):
     #     return one_hot
 
     # set the target class as one others as zero. use this vector for back prop added by Lezi
+
     def encode_one_hot_batch(self, z, mu, logvar, mu_avg, logvar_avg):
         one_hot_batch = torch.FloatTensor(z.size()).zero_()
+        # They return mu here. Probably one_hot_batch is not implemented yet.
         return mu
 
     def forward(self, x):
@@ -49,17 +52,23 @@ class PropBase(object):
         self.model.zero_grad()
         z = self.model.reparameterize_eval(mu, logvar).cuda()
         one_hot = self.encode_one_hot_batch(z, mu, logvar, mu_avg, logvar_avg)
-
         if self.cuda:
             one_hot = one_hot.cuda()
         flag = 2
         if flag == 1:
+            print('Flag is 1 (somehow?)')
             self.score_fc = torch.sum(F.relu(one_hot.cuda() * mu))
         else:
             self.score_fc = torch.sum(one_hot.cuda())
         self.score_fc.backward(retain_graph=True)
 
     def get_conv_outputs(self, outputs, target_layer):
+        """
+        Retrieves model output for a specific module from a dictionary.
+        Inputs:
+            outputs - Dictionary to retrieve values from (forward or backward)
+            target_layer - Specific module for which to retrieve values
+        """  
         for key, value in outputs.items():
             for module in self.model.named_modules():
                 if id(module[1]) == key:
@@ -71,32 +80,54 @@ class GradCAM(PropBase):
 
     def set_hook_func(self):
         def func_b(module, grad_in, grad_out):
+            """
+            Hook call function that stores the backward pass gradients for every 
+            network module in a dictionary.
+            """
             self.outputs_backward[id(module)] = grad_out[0].cpu()
 
         def func_f(module, input, f_output):
+            """
+            Hook call function that stores the forward pass output for every 
+            network module in a dictionary.
+            """
             self.outputs_forward[id(module)] = f_output
 
+        # Loop over all layers in the network and store outputs of forward 
+        # and backward passes
         for module in self.model.named_modules():
             module[1].register_backward_hook(func_b)
             module[1].register_forward_hook(func_f)
 
     def normalize(self, grads):
+        """
+        Applies L2 normalization to the gradients
+        """        
         l2_norm = torch.sqrt(torch.mean(torch.pow(grads, 2))) + 1e-5
         return grads / l2_norm.item()
 
+
     def compute_gradient_weights(self):
+        """
+        Applies the GAP operation to the gradients to obtain weights alpha.
+        """  
         self.grads = self.normalize(self.grads.squeeze())
+        # Get height and width of attention maps
         self.map_size = self.grads.size()[2:]
         self.weights = nn.AvgPool2d(self.map_size)(self.grads)
 
+
     def generate(self):
-        # get gradient
+        """
+        Generates attention map from gradients. 
+        """  
+        # Retrieve gradients of backward pass for target layer
         self.grads = self.get_conv_outputs(
             self.outputs_backward, self.target_layer)
-        # compute weithts based on the gradient
+        # compute weigths based on the gradient
         self.compute_gradient_weights()
 
-        # get activation
+        # Retrieve output of forward pass for target layer and set as activation
         self.activiation = self.get_conv_outputs(
             self.outputs_forward, self.target_layer)
 
