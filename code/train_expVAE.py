90bd242a353de96dd74ece4622f225bf6ad3bd8b
Vivien van Veldhuizen <vivien.van.veldhuizen@hotmail.com>
2021-01-25 15:24:40 +0100

    added tensorboard to train file

--- code/train_expVAE.py
+++ code/train_expVAE.py
@@ -19,6 +19,8 @@ import OneClassMnist
 import Ped1_loader
 import MVTec_loader as mvtec
 import matplotlib.pyplot as plt
+from tensorboardX import SummaryWriter
+# Run the folloring command to acces tensorboard: tensorboard --logdir runs
 
 def loss_function(recon_x, x, mu, logvar):
     """
@@ -118,6 +120,7 @@ def main(args):
         args - Namespace object from the argument parser
     """
     print("Device is", device)
+    writer = SummaryWriter()
 
     # Seed everything
     torch.manual_seed(args.seed)
@@ -194,6 +197,9 @@ def main(args):
 
         train_loss = train(model, train_loader, optimizer, args)
         test_loss = test(model, test_loader,args)
+
+        writer.add_scalar('Train Loss', train_loss, epoch)
+        writer.add_scalar('Test Loss', test_loss, epoch)
 
         print('Epoch [%d/%d] loss: %.3f val_loss: %.3f' % (epoch + 1, args.epochs, train_loss, test_loss))
         print(f"Lr: {optimizer.param_groups[0]['lr']}")
