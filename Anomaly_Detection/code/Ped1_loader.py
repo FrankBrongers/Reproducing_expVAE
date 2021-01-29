dcac6924b0def038d7e06254c4eecbd4b4079fda
watermeleon <leoneshuijs@gmail.com>
2021-01-29 20:08:28 +0100

    copied

---
+++ Anomaly_Detection/code/Ped1_loader.py
@@ -0,0 +1,127 @@
+import os
+import torch
+import torch.utils.data as data
+import torchvision.transforms as transforms
+import tarfile
+from tqdm import tqdm
+import urllib.request
+
+from PIL import Image
+
+"""
+Based on https://github.com/maksimbolonkin/video_anomaly_detection_pytorch/blob/master/ucsd_dataset.py
+"""
+
+URL = 'http://www.svcl.ucsd.edu/projects/anomaly/UCSD_Anomaly_Dataset.tar.gz'
+
+class UCSDAnomalyDataset(data.Dataset):
+    def __init__(self, root_path='./data', train=True, resize=96, download=True):
+        super(UCSDAnomalyDataset, self).__init__()
+
+        self.root_path = root_path
+        self.dataset_mean = 0.3750352255196134
+        self.dataset_std = 0.20129592430286292
+
+        if train:
+            self.images_dir = os.path.join(root_path, 'UCSD_Anomaly_Dataset.v1p2/UCSDped1/Train')
+        else:
+            self.images_dir = os.path.join(root_path, 'UCSD_Anomaly_Dataset.v1p2/UCSDped1/Test')
+
+        # download dataset if not exist
+        if download:
+            self.download()
+
+        # Get all x folder dirs
+        x_dirs = [d for d in os.listdir(self.images_dir) if os.path.isdir(os.path.join(self.images_dir, d)) and d[-2:] != 'gt']
+
+        # Get all y folder dirs, None when no y in folder
+        y_dirs = []
+        for d in x_dirs:
+            if os.path.isdir(os.path.join(self.images_dir,(d + '_gt'))):
+                y_dirs.append(d + '_gt')
+            else:
+                y_dirs.append(None)
+
+        self.x_samples = []
+        self.y_samples = []
+
+        # Construct a list of al possible x's and y's --> None if no target available
+        for x_d, y_d in zip(x_dirs, y_dirs):
+            for i in range(1, 201):
+                self.x_samples.append(os.path.join(self.images_dir, x_d, '{0:03d}.tif'.format((i))))
+
+                # If available add x mask dir
+                if y_d:
+                    self.y_samples.append(os.path.join(self.images_dir, y_d, '{0:03d}.bmp'.format((i))))
+                else:
+                    self.y_samples.append(None)
+
+        self.pil_transform = transforms.Compose([
+                    transforms.Resize((resize, resize)),
+                    transforms.Grayscale(),
+                    transforms.ToTensor(),
+                    ])
+
+    def unnormalize(self, input):
+        return input.mul_(self.dataset_std).add_(self.dataset_mean)
+
+
+    def __getitem__(self, index):
+        # Get x
+        with open(self.x_samples[index], 'rb') as file:
+            x_frame = Image.open(file)
+            x_frame = self.pil_transform(x_frame)
+        # Get y
+        if self.y_samples[index]:
+            with open(self.y_samples[index], 'rb') as file:
+                y_frame = Image.open(file)
+                y_frame = self.pil_transform(y_frame)
+
+        else:
+            y_frame = torch.zeros_like(x_frame)
+
+        assert x_frame.shape == y_frame.shape, 'x and y shape do not match'
+        return x_frame, y_frame
+
+
+    def __len__(self):
+        return len(self.x_samples)
+
+
+    def download(self):
+        """Download dataset if not exist"""
+
+        if not os.path.exists(self.images_dir):
+            tar_file_path = self.root_path + 'ucsd_ped1.tar.xz'
+            if not os.path.exists(tar_file_path):
+                download_url(URL, tar_file_path)
+            print('unzip downloaded dataset: %s' % tar_file_path)
+            tar = tarfile.open(tar_file_path)
+            tar.extractall(self.root_path)
+            tar.close()
+        return
+
+
+class DownloadProgressBar(tqdm):
+    def update_to(self, b=1, bsize=1, tsize=None):
+        if tsize is not None:
+            self.total = tsize
+        self.update(b * bsize - self.n)
+
+
+def download_url(url, output_path):
+    with DownloadProgressBar(unit='B', unit_scale=True, miniters=1, desc=url.split('/')[-1]) as t:
+        urllib.request.urlretrieve(url, filename=output_path, reporthook=t.update_to)
+
+
+# Test main
+if __name__ == "__main__":
+    dataset = UCSDAnomalyDataset('data/', train=False)
+    print(f"Dataset length: {dataset.__len__()}")
+
+    for i in range(0, 7199):
+        I = dataset.__getitem__(i)
+        if i % 500 == 0:
+            print(I[0].min(), I[0].max())
+            I = dataset.unnormalize(I[0])
+            print(I.min(), I.max())
---
+++ Anomaly_Detection/code/train_expVAE.py
@@ -0,0 +1,280 @@
+import argparse
+import torch
+import torch.optim as optim
+from torch.nn import functional as F
+from torchvision import transforms
+from torchvision.utils import save_image
+from torchvision.utils import make_grid
+
+import os
+import shutil
+import numpy as np
+
+from models.vanilla_mnist import ConvVAE_mnist
+from models.vanilla_ped1 import ConvVAE_ped1
+from models.resnet18_3 import ResNet18VAE_3
+
+
+import OneClassMnist
+import Ped1_loader
+import MVTec_loader as mvtec
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+# from tensorboardX import SummaryWriter
+# Run the folloring command to acces tensorboard: tensorboard --logdir runs
+# from torchvision import transforms as T
+
+
+def loss_function(recon_x, x, mu, logvar, ):
+    """
+    Calculates the reconstruction (binary cross entropy) and regularization (KLD) losses to form the total loss of the VAE.
+    Inputs:
+        recon_x - Batch of reconstructed images of shape [B,C,H,W].
+        x - Batch of original input images of shape [B,C,H,W].
+        mu - Mean of the posterior distributions.
+        log_var - Log standard deviation of the posterior distributions.
+    """
+    B = recon_x.shape[0]
+
+    rec_loss = F.binary_cross_entropy(recon_x.view(B, -1), x.view(B, -1), reduction='sum').div(B)
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp()).div(B)
+
+    return rec_loss + KLD
+
+
+def train(model, train_loader, optimizer, args):
+    """
+    Function for training a model on a dataset. Train for one epoch.
+    Inputs:
+        model - VAE model to train
+        train_loader - Data Loader for the dataset you want to train on
+        optimizer - The optimizer used to update the parameters
+    Outputs:
+        train_loss - Averaged total loss
+    """
+    model.train()
+    train_loss = 0
+
+    for batch_idx, (data, _) in enumerate(train_loader):
+
+        data = data.to(device)
+
+        optimizer.zero_grad()
+
+        recon_batch, mu, logvar = model(data)
+
+        loss = loss_function(recon_batch, data, mu, logvar)
+        train_loss += loss.item()
+
+        loss.backward()
+        optimizer.step()
+
+    train_loss /= len(train_loader.dataset)/args.batch_size
+    return train_loss
+
+
+def test(model, test_loader, args):
+    """
+    Function for testing the model on a test dataset. Test for one epoch.
+    Inputs:
+        model - VAE model to train
+        test_loader - Data Loader for the dataset you want to test on
+    Outputs:
+        test_loss - Averaged total loss on test set
+    """
+    model.eval()
+    test_loss = 0
+
+    with torch.no_grad():
+        for batch_idx, (data, _) in enumerate(test_loader):
+            data = data.to(device)
+            recon_batch, mu, logvar = model(data)
+            test_loss += loss_function(recon_batch, data, mu, logvar).item()
+
+    test_loss /= len(test_loader.dataset)/args.batch_size
+    return test_loss
+
+
+def save_checkpoint(state, is_best, outdir, args):
+    """
+    Function for saving pytorch model checkpoints.
+    Inputs:
+        state - state of the model containing current epoch, model, optimizer and loss
+        is_best - boolean stating if model has best test loss so far
+        outdir - directory to save checkpoints
+    """
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    if args.dataset == "mvtec_ad":
+            checkpoint_file = os.path.join(outdir, f"{state['model']}_mvtecClass_"+str(args.one_class) +"_checkpoint.pth")
+            best_file = os.path.join(outdir, f"{state['model']}_mvtecClass_"+str(args.one_class)+"_best.pth")
+    else:
+        checkpoint_file = os.path.join(outdir, f"{state['model']}_checkpoint.pth")
+        best_file = os.path.join(outdir, f"{state['model']}_best.pth")
+    torch.save(state, checkpoint_file)
+    if is_best:
+        shutil.copyfile(checkpoint_file, best_file)
+
+def main(args):
+    """
+    Main Function for the full training & evaluation loop of the VAE model.
+    Inputs:
+        args - Namespace object from the argument parser
+    """
+    print("Device is", device)
+
+    # Seed everything
+    torch.manual_seed(args.seed)
+
+    # Load dataset
+    if args.dataset == 'mnist':
+        imshape = [64, 1, 28, 28]
+        train_dataset = OneClassMnist.OneMNIST('./data', args.one_class, train=True, download=True, transform=transforms.ToTensor())
+    elif args.dataset == 'ucsd_ped1':
+        imshape = [64, 1, 100, 100]
+        train_dataset = Ped1_loader.UCSDAnomalyDataset('./data/', train=True, resize=imshape[-1])
+    elif args.dataset == 'mvtec_ad':
+        imshape = [64, 3, 256, 256 ]
+        class_name = mvtec.CLASS_NAMES[args.one_class]
+        train_dataset = mvtec.MVTecDataset(class_name=class_name, is_train=True, grayscale=False, root_path=args.data_path)
+
+
+    kwargs = {'num_workers': args.num_workers, 'pin_memory': True} if torch.cuda.is_available() else {}
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=args.batch_size, shuffle=True, **kwargs)
+
+    # Select a model architecture
+    if args.model == 'vanilla':
+        model = ConvVAE_mnist(args.latent_size).to(device)
+    elif args.model == 'vanilla_ped1':
+        model = ConvVAE_ped1(args.latent_size, args.image_size, [1, 64, 128, 256], batch_norm=False).to(device)
+    elif args.model == 'resnet18_3':
+        model = ResNet18VAE_3(args.latent_size, x_dim = imshape[-1], nc = imshape[1]).to(device)
+
+    # Create optimizer and scheduler
+    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
+
+    # process optional milestones parameter
+    milestones = args.scheduler
+    if not isinstance(milestones, list) : milestones = [milestones]
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=milestones, gamma=0.1)
+
+    # Initialize assesment variables
+    start_epoch = 0
+    best_train_loss = np.finfo('f').max
+    losses = []
+
+    # Optionally resume from a checkpoint
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print('=> loading checkpoint %s' % args.resume)
+            checkpoint = torch.load(args.resume)
+            start_epoch = checkpoint['epoch'] + 1
+            best_train_loss = checkpoint['best_train_loss']
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            print('=> loaded checkpoint %s' % args.resume)
+        else:
+            print('=> no checkpoint found at %s' % args.resume)
+
+
+    # Training and testing
+    for epoch in range(start_epoch, args.epochs):
+        if args.vae_testsave == True:
+            with torch.no_grad():
+                save_dir = args.result_dir
+                if not os.path.exists(save_dir):
+                    os.makedirs(save_dir)
+                testim =  next(iter(train_loader))[0][0][None,:].to(device)
+                gen_testim = model(testim)[0]
+
+                combi = make_grid([testim[0].cpu(), gen_testim[0].cpu()], padding=100)
+                save_image(combi.cpu(), os.path.join(save_dir,"combi_"+ str(epoch) + '.png'))
+
+        train_loss = train(model, train_loader, optimizer, args)
+
+        # writer.add_scalar('Train Loss', train_loss, epoch)
+        # writer.add_scalar('Test Loss', test_loss, epoch)
+
+        print('Epoch [%d/%d] loss: %.3f ' % (epoch + 1, args.epochs, train_loss))
+        print(f"Lr: {optimizer.param_groups[0]['lr']}")
+
+        # save training loss plot
+        losses.append(train_loss)
+        plt.plot(losses)
+        plt.savefig(args.result_dir + "/loss_" + str(args.model)+ ".png")
+
+        # Check if model is good enough for checkpoint to be created
+        is_best = train_loss < best_train_loss
+        best_train_loss = min(train_loss, best_train_loss)
+        save_checkpoint({
+            'epoch': epoch,
+            'best_train_loss': best_train_loss,
+            'state_dict': model.state_dict(),
+            'optimizer': optimizer.state_dict(),
+            'model' : args.model,
+        }, is_best, os.path.join('./',args.ckpt_dir), args)
+
+        # Visualize sample validation result
+        with torch.no_grad():
+            sample = torch.randn(64, args.latent_size).to(device)
+            sample = model.decode(sample).cpu()
+            save_dir = args.result_dir
+            if not os.path.exists(save_dir):
+                os.makedirs(save_dir)
+            save_image(sample.view(imshape), os.path.join(save_dir,str(args.model) +'sample_' + str(epoch) + '.png'))
+        scheduler.step()
+
+if __name__ == '__main__':
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    parser = argparse.ArgumentParser(description='Explainable VAE')
+
+    # Path options
+    parser.add_argument('--result_dir', type=str, default=None, metavar='DIR',
+                        help='output directory')
+    parser.add_argument('--ckpt_dir', type=str, default='ckpt', metavar='DIR',
+                        help='ckpt directory')
+    parser.add_argument('--resume', default=False, type=str, metavar='PATH',
+                        help='path to latest checkpoint (default: None')
+
+    # Training parameters
+    parser.add_argument('--batch_size', type=int, default=32, metavar='N',
+                        help='input batch size for training (default: 128)')
+    parser.add_argument('--learning_rate', default=0.0001, type=float,
+                        help='Learning rate to use')
+    parser.add_argument('--epochs', type=int, default=512, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--num_workers', default=4, type=int,
+                        help='Number of workers to use in the data loaders.')
+
+    # Model parameters
+    parser.add_argument('--model', type=str, default='vanilla_ped1',
+                        help='select one of the following models: vanilla_mnist, resnet18, vanilla_ped1')
+    parser.add_argument('--latent_size', type=int, default=32, metavar='N',
+                        help='latent vector size of encoder')
+    parser.add_argument('--image_size', type=int, default=100,
+                        help='Select an image size')
+    parser.add_argument('--data_path', type=str, default='./data', metavar='DIR',
+                        help='directory for storing and finding the dataset')
+
+    # Dataset parameters
+    parser.add_argument('--dataset', type=str, default='ucsd_ped1',
+                        help='select one of the following datasets: mnist, ucsd_ped1, mvtec_ad')
+    parser.add_argument('--one_class', type=int, default=1, metavar='N',
+                        help='inlier digit for one-class VAE training')
+    parser.add_argument('--vae_testsave', type=bool, default=False,
+                        help='save input output image of VAE during training')
+    parser.add_argument('--scheduler', type=int, nargs="+", default=-1,
+                        help='steps for the learning scheduler')
+    args = parser.parse_args()
+
+    # If no argument for result directory is specified, set it to data and model name
+    if args.result_dir is None:
+        args.result_dir = './train_results/{}_{}'.format(args.dataset, args.model)
+
+    main(args)
